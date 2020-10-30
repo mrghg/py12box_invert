1a3b5479c113c39114f64becfd25590067afa8fa
Matt Rigby <matt.rigby@bristol.ac.uk>
2020-10-30 16:00:04 +0000

    Minor edits

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from acrg.acrg_obs import get_single_site
 import xarray as xr
 import pandas as pd
+import py12box
+import numpy as np
 
 
 def monthly_baselines(site, species, box):
@@ -17,6 +19,8 @@ def monthly_baselines(site, species, box):
         12-box model box number (0 - 11)
     """
 
+    #TODO: This function needs moving into a different repo, as it requires ACRG repo
+    
     dataset = xr.concat(get_single_site(site, species),
                         dim="time").sortby("time")
 
@@ -82,6 +86,60 @@ def obs_read(species, project_path, case):
     """
 
     df = pd.read_csv(project_path / case / f"obs_{species}.csv", header=[0, 1, 2],
-                     skipinitialspace=True, index_col=0)
+                     skipinitialspace=True, index_col=0,
+                     parse_dates=[0])
 
     return df
+
+
+def approx_initial_conditions(species, project_path, case,
+                              ic):
+    """
+    Spin up the model to approximate initial conditions
+    
+    Parameters
+    ----------
+    species : str
+        Species string
+    project_path: pathlib path
+        Path to project
+    case: str
+        Case folder within project
+    ic: list
+        Initial mole fraction at four surface boxes
+    """
+    
+    if len(ic) != 4:
+        raise("Initial conditions must be 4 elements (surface boxes, ordered N - S)")
+        
+    mol_mass, OH_A, OH_ER = setup.get_species_parameters(species)
+    time, emissions, ic, lifetime = setup.get_case_parameters(project_dir, case, species)
+    
+    
+    
+    i_t, i_v1, t, v1, OH, Cl, temperature = setup.get_model_parameters(int(len(time) / 12))
+    F = setup.transport_matrix(i_t, i_v1, t, v1)
+
+    c_month, burden, emissions_out, losses, lifetimes = \
+        core.model(ic=ic, q=emissions,
+                   mol_mass=mol_mass,
+                   lifetime=lifetime,
+                   F=F,
+                   temp=temperature,
+                   cl=Cl, oh=OH,
+                   arr_oh = np.array([OH_A, OH_ER]))
+    
+
+def decimal_date(date):
+    '''
+    Calculate decimal date from pandas DatetimeIndex
+    
+    Parameters
+    ----------
+    date : pandas DatetimeIndex
+    
+    '''
+    
+    days_in_year = np.array([[365., 366.][int(ly)] for ly in date.is_leap_year])
+    
+    return (date.year + (date.dayofyear-1.)/days_in_year + date.hour/24.).to_numpy()
