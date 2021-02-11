9db470221afcf355647e02d14d55aa2a6acca1d9
Luke Western <luke.western@bristol.ac.uk>
2021-02-11 12:17:49 +0000

    Now infers yearly emissions

--- core.py
+++ core.py
@@ -1,43 +1,87 @@
 import numpy as np
 import pandas as pd
-from py12box import setup, core
+from py12box.py12box import startup, core,  model
 from tqdm import tqdm
+from py12box_invert import utils
 
-def flux_sensitivity(project_path, case, species, ic=None, input_dir=None, styear=None):
+def fwd_model(ic, emissions, mol_mass, lifetime, F, temperature, cl, oh, oh_a, oh_er):
     """
+    Foward model call of box model
+    Inputs:
+        ic          initial conditions
+        emissons    monthly emissions in 4 surface boxes
+        mol_mass    molar mass of species
+        lifetime    lifetime of species in each box
+        F           Transport matrix
+        temperature Temperature in each box in each month
+        cl          chlorine loss
+        oh          oh loss
+        oh_a        oh_a loss
+        oh_b        oh_b loss
+    Returns:
+        mf          monthly mole fraction in each box
+        burden      burden of species in each box
+        losses      loss in each box
+        lifetimes   calculates lifetimes in each box
+    """
+    mf, burden, emissions_out, losses, lifetimes = \
+    core.model(ic=ic, q=emissions,
+                mol_mass=mol_mass,
+                lifetime=lifetime,
+                F=F,
+                temp=temperature,
+                cl=cl, oh=oh,
+                arr_oh=np.array([oh_a, oh_er]))
+    return mf, burden, emissions_out, losses, lifetimes
 
+def fwd_model_inputs(project_path, case, species):
+    """
+    Get inputs to 12-box model
+    """
+    #mol_mass, oh_a, oh_er = setup.get_species_parameters(species)
+    #time, emissions, ic, lifetime = setup.get_case_parameters(project_path, case, species)
+    #i_t, i_v1, t, v1, oh, cl, temperature = setup.get_model_parameters(int(len(time) / 12))
+    #F = setup.transport_matrix(i_t, i_v1, t, v1)
+    mod = model.Model(species, project_path)
+    return mod #time, ic, emissions, mol_mass, lifetime, F, temperature, cl, oh, oh_a, oh_er
+
+def flux_sensitivity(project_path, case, species, ic0=None):
+    """
+    Derive linear yearly flux sensitivities
+    
     Parameters
     ----------
     project_path
     case
     species
+    ic0 (optional): Surface initial condition if estimating ic
 
     Returns
     -------
 
     """
+#     time, ic, emissions, mol_mass, lifetime, F, temperature, cl, oh, oh_a, oh_er = \
+#             fwd_model_inputs(project_path, case, species)
+    mod = \
+             fwd_model_inputs(project_path, case, species)
     
-    mol_mass, oh_a, oh_er = setup.get_species_parameters(species)
-    time, emissions, ic0, lifetime = setup.get_case_parameters(project_path, case, species)
-    if not ic:
-        ic = np.copy(ic0)
-    if input_dir:
-        styear = int(time[0])
-        i_t, i_v1, t, v1, oh, cl, temperature = setup.get_model_parameters(int(len(time) / 12), input_dir=input_dir, styear=styear)
-    else:
-        i_t, i_v1, t, v1, oh, cl, temperature = setup.get_model_parameters(int(len(time) / 12))
-    F = setup.transport_matrix(i_t, i_v1, t, v1)
-
-    mf_ref, burden, emissions_out, losses, lifetimes = \
-        core.model(ic=ic, q=emissions,
-                   mol_mass=mol_mass,
-                   lifetime=lifetime,
-                   F=F,
-                   temp=temperature,
-                   cl=cl, oh=oh,
-                   arr_oh=np.array([oh_a, oh_er]))
-
+    if ic0 is not None:
+        if len(ic0) == 4:
+            ic = utils.approx_initial_conditions(species, project_path, case,
+                                  ic0)
+            mod.ic = ic
+        else:
+            print("ic0 does not have only 4 surface boxes. Ignoring.")
+        
     
+#     mf_ref, burden, emissions_out, losses, lifetimes = \
+#             fwd_model(ic, emissions, mol_mass, lifetime, F, temperature, cl, oh, oh_a, oh_er)
+    mod.run()
+    mf_ref = mod.mf
+    emissions = mod.emissions
+    
+    if len(emissions) % 12:
+        raise Exception("Emissions must contain whole years")
     nyear = int(len(emissions)/12)
     sensitivity = np.zeros((len(mf_ref[:,:4].flatten()), int(nyear*4)))
     
@@ -45,30 +89,75 @@ def flux_sensitivity(project_path, case, species, ic=None, input_dir=None, styea
         for bi in range(4):
 
             emissions_perturbed = emissions.copy()
-            emissions_perturbed[mi*12:(12*mi+12), bi] +=1 #*= 2.
+            emissions_perturbed[mi*12:(12*mi+12), bi] +=1
             
-            mf_perturbed, burden, emissions_out, losses, lifetimes = \
-                core.model(ic=ic, q=emissions_perturbed,
-                           mol_mass=mol_mass,
-                           lifetime=lifetime,
-                           F=F,
-                           temp=temperature,
-                           cl=cl, oh=oh,
-                           arr_oh=np.array([oh_a, oh_er]))
+            mod.emissions = emissions_perturbed
+            mod.run(verbose=False)
+            mf_perturbed = mod.mf
+            #mf_perturbed, burden, emissions_out, losses, lifetimes = \
+            #        fwd_model(ic, emissions_perturbed, mol_mass, lifetime, F, temperature, cl, oh, oh_a, oh_er)
 
             sensitivity[:, 4*mi + bi] = (mf_perturbed[:,:4].flatten() - mf_ref[:,:4].flatten()) / 1.
     
-    return sensitivity, mf_ref[:,:4].flatten()
+    return sensitivity, mf_ref[:,:4].flatten(), emissions, mod.time
 
 
+def inversion_analytical(y, H, x_a, R, P):
+    """
+    Perform a Gaussian analytical inversion assuming linearity.
+    The inferred value 'x_hat' is the difference in emissions from the reference run.
+    
+    TODO:
+        This should really calculate global totals using off-diagonals
+    """
+    R_inv = np.linalg.inv(R)
+    P_inv = np.linalg.inv(P)
+    x_hat = np.linalg.inv(H.T @ R_inv @ H + P_inv) @ (H.T @ R_inv @ y + P_inv @ x_a)
+    P_hat = np.linalg.inv(H.T @ R_inv @ H + P_inv)
+    return x_hat, P_hat
 
+def annual_means(x_hat, P_hat, emis_ref,  freq="yearly"):
+    """
+    Derive annual mean emissions from inversion output
+    """
+    
+    if freq == "yearly":
+        x_mnth = np.sum(np.repeat(x_hat.reshape(int(len(x_hat)/4),4),12, axis=0)+ emis_ref, axis=1)
+        x_out = np.mean(x_mnth.reshape(-1, 12), axis=1)
+        x_sd_out = np.zeros(len(x_out))
+        j = 0
+        for i in range(0,len(x_hat),4):
+            x_sd_out[j] = np.sqrt(np.sum(P_hat[i:(i+4),i:(i+4)]))
+            j+=1
+        
+        
+    return x_out, x_sd_out
+
+def run_inversion(project_path, case, species, ic0=None,  emissions_sd=None):
+    """
+    Run inversion for 12-box model to estimate monthly means from 4 surface boxes.
+    """
+    #Get obs
+    obsdf =  utils.obs_read(species, project_path, case)
+    obstime = utils.decimal_date(obsdf.index)
+    #Box obs
+    mf_box, mf_var_box = utils.obs_box(obsdf)
+    #Get sensitivities
+    sensitivity, mf_ref, emis_ref, time = flux_sensitivity(project_path, species, case, ic0=ic0)
+    # Pad obs to senstivity
+    obs, obs_sd = utils.pad_obs(mf_box, mf_var_box, time, obstime)
+    #Get matrices for inversion 
+    H, y, R, P, x_a = utils.inversion_matrices(obs, sensitivity, mf_ref, obs_sd, emissions_sd)
+    #Do inversion
+    x_hat, P_hat = inversion_analytical(y, H, x_a, R, P)
+    return x_hat, P_hat, emis_ref
 
 if __name__ == "__main__":
 
     from pathlib import Path
     import matplotlib.pyplot as plt
 
-    H = flux_sensitivity(Path("/home/chxmr/code/py12box_invert/data/AGAGE"), "CFC-11", "CFC-11")
+    H, mf_ref, emis_ref = flux_sensitivity(Path("/home/lw13938/work/py12box-invert/data/example"), "CFC-11", "CFC-11")
 
     print(H[0, 0])
 
--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
-#from acrg.acrg_obs import get_single_site
+from acrg_obs import get_single_site
 import xarray as xr
 import pandas as pd
-import py12box
+#import py12box
 import numpy as np
-from py12box import setup, core
-
+from py12box.py12box import startup, core
+from py12box_invert import core as invcore
 
 def monthly_baselines(site, species, box):
     """
@@ -92,6 +92,19 @@ def obs_read(species, project_path, case):
 
     return df
 
+def obs_box(obsdf):
+    """
+    Box up obs data into surface boxes
+    Also returns measurement error as root-square-sum of measurment variability
+    Q: Should I weight these by latitude?!
+    Q: Currently more than one measurement in box then uncertatinty is higher – not really true.
+    """
+    mf_box = np.zeros((4, len(obsdf.index))) 
+    mf_var_box = np.zeros((4, len(obsdf.index))) 
+    for sb in range(4):
+        mf_box[sb,:] = obsdf.xs("mf",level="var", axis=1).xs(str(sb),level="box", axis=1).mean(axis=1, skipna=True)
+        mf_var_box[sb,:] = obsdf.xs("mf_variability",level="var", axis=1).xs(str(sb),level="box", axis=1).apply(np.square).apply(np.nansum, axis=1).apply(np.sqrt)
+    return mf_box, mf_var_box
 
 def approx_initial_conditions(species, project_path, case,
                               ic0):
@@ -113,20 +126,23 @@ def approx_initial_conditions(species, project_path, case,
     if len(ic0) != 4:
         raise("Initial conditions must be 4 elements (surface boxes, ordered N - S)")
         
-    mol_mass, OH_A, OH_ER = py12box.setup.get_species_parameters(species)
-    time, emissions, ic, lifetime = py12box.setup.get_case_parameters(project_path, case, species)
+    #mol_mass, OH_A, OH_ER = py12box.setup.get_species_parameters(species)
+    #time, emissions, ic, lifetime = py12box.setup.get_case_parameters(project_path, case, species)
+    #i_t, i_v1, t, v1, OH, Cl, temperature = py12box.setup.get_model_parameters(int(len(time) / 12))
+    #F = setup.transport_matrix(i_t, i_v1, t, v1)
     
-    i_t, i_v1, t, v1, OH, Cl, temperature = py12box.setup.get_model_parameters(int(len(time) / 12))
-    F = setup.transport_matrix(i_t, i_v1, t, v1)
+    mod = invcore.fwd_model_inputs(project_path, case, species)
+    mod.run(verbose=False)
+    c_month = mod.mf
 
-    c_month, burden, emissions_out, losses, lifetimes = \
-        core.model(ic=ic, q=emissions,
-                   mol_mass=mol_mass,
-                   lifetime=lifetime,
-                   F=F,
-                   temp=temperature,
-                   cl=Cl, oh=OH,
-                   arr_oh = np.array([OH_A, OH_ER]))
+#     c_month, burden, emissions_out, losses, lifetimes = \
+#         core.model(ic=ic, q=emissions,
+#                    mol_mass=mol_mass,
+#                    lifetime=lifetime,
+#                    F=F,
+#                    temp=temperature,
+#                    cl=Cl, oh=OH,
+#                    arr_oh = np.array([OH_A, OH_ER]))
     
     #Take final spun up value and scale each semi-hemisphere to surface boxes.
     return c_month[-1,:] * np.tile(ic0[:4]/c_month[-1,:4],3)
@@ -144,3 +160,42 @@ def decimal_date(date):
     days_in_year = np.array([[365., 366.][int(ly)] for ly in date.is_leap_year])
     
     return (date.year + (date.dayofyear-1.)/days_in_year + date.hour/24.).to_numpy()
+
+def adjust_emissions_time(time):
+    """
+    Currently emissions date is not calendar months but 1/12th year fractions
+    Adjust to align with calendar decimal years
+    """
+    time_start = str(int(time[0]))+"-"+str(int(np.round((time[0]-int(time[0])+1./12.)*12.))).zfill(2)
+    time_end = str(int(time[-1]))+"-"+str(int(np.round((time[-1]-int(time[-1])+1./12.)*12.))).zfill(2)
+    dt_time = pd.date_range(time_start,time_end, freq="MS")
+    return decimal_date(dt_time)
+
+
+def pad_obs(mf_box, mf_var_box, time,obstime):
+    """
+    Pad the obs data with NaNs to cover period covered by input emissions
+    """
+    emis_time = adjust_emissions_time(time)
+    obs_df = pd.DataFrame(index=obstime, data=mf_box.T,columns=["0","1","2","3"]).reindex(emis_time)
+    obs_sd_df = pd.DataFrame(index=obstime, data=mf_var_box.T,columns=["0","1","2","3"]).reindex(emis_time)
+    obs = obs_df.values.flatten()
+    obs_sd = obs_sd_df.values.flatten()
+    return obs, obs_sd
+
+def inversion_matrices(obs, sensitivity, mf_ref, obs_sd, P_sd):
+    """
+    Drop sensitivities to no observations and set up matrices
+    Prior uncertainty on emissions is hardwired to 100.
+    """
+    wh_obs = np.isfinite(obs)
+    H = sensitivity[wh_obs,:]
+    y = obs[wh_obs] - mf_ref[wh_obs]
+    R = np.diag(obs_sd[wh_obs]**2)
+    if not P_sd:
+        # If no emissions std is given default to 100 Gg/yr
+        print("Prior emissions uncertainty defaulting to 100 Gg/box/yr")
+        P_sd = np.ones(H.shape[1])*100
+    P = np.diag(P_sd**2)
+    x_a = np.zeros(H.shape[1])
+    return H, y, R, P, x_a
