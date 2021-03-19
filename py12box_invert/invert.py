ad15f694d90b9136f51ef9ba436cd55c0a9d0155
Matt Rigby <matt.rigby@bristol.ac.uk>
2021-03-19 06:17:51 +0000

    Tidied up sensitivity method

--- py12box_invert/invert.py
+++ py12box_invert/invert.py
@@ -51,21 +51,15 @@ class Invert:
         self.mod_prior = Prior_model(self.mod)
 
 
-    def calc_sensitivity(self, freq="monthly"):
+    def flux_sensitivity(self, freq="monthly"):
         """
         Derive linear yearly flux sensitivities
         
             Parameters:
-                project_path (pathlib path): Path to project
-                species (str)              : Species name
-                ic0 (array)                : Initial conditions for 4 surface boxes
                 freq (str, optional)       : Frequency to infer ("monthly", "quarterly", "yearly")
                                             Default is monthly.
             Returns:
                 Linear sensitivity to emissions
-                Reference mole fraction
-                a priori emissions
-                time of emissions
 
         """
 
@@ -73,63 +67,33 @@ class Invert:
         if freq not in ["monthly", "quarterly", "yearly"]:
             raise Exception('Frequency must be "monthly", "quarterly" or "yearly"')
         
-        meanfreq={"monthly":1, "quarterly":3, "yearly":12}
+        freq_months={"monthly":1, "quarterly":3, "yearly":12}[freq]
         
-        #emis0 = self.mod.emissions.T.reshape(-1,meanfreq[freq]).mean(1).reshape(4,-1).T.flatten()
+        nmonths = len(self.mod_prior.time)
 
-        # emissions = np.zeros_like(self.mod_prior.emissions)
-                
-        ntime = len(self.mod_prior.time)
-
-        if ntime % 12:
+        if nmonths % 12:
             raise Exception("Emissions must contain whole years")
-            
-        if freq == "yearly":
-            nyear = int(ntime/12)
-            self.sensitivity = np.zeros((ntime*4, nyear*4))
+        
+        # number of discrete periods over which emissions sensitivity is calculated
+        nsens = int(nmonths/freq_months)
 
-            for mi in tqdm(range(nyear)):
-                for bi in range(4):
+        # empty sensitivity matrix. Factors of 4 are for surface boxes 
+        # (mole fraction in rows, emissions in columns)
+        self.sensitivity = np.zeros((nmonths*4, nsens*4))
 
-                    emissions_perturbed = self.mod_prior.emissions.copy()
-                    emissions_perturbed[mi*12:(12*mi+12), bi] +=1
+        for ti in tqdm(range(nsens)):
+            for bi in range(4):
 
-                    self.mod.emissions = emissions_perturbed.copy()
-                    self.mod.run(verbose=False)
-                    
-                    self.sensitivity[:, 4*mi + bi] = (self.mod.mf[:,:4].flatten() - self.mod_prior.mf[:,:4].flatten()) / 1.
+                # Perturb emissions uniformly throughout specified time period
+                emissions_perturbed = self.mod_prior.emissions.copy()
+                emissions_perturbed[ti*freq_months:freq_months*(ti+1), bi] += 1
+                self.mod.emissions = emissions_perturbed.copy()
 
-    #     elif freq == "monthly":
-    #         nmonth = len(emissions)
-    #         sensitivity = np.zeros((len(mf_ref[:,:4].flatten()), int(nmonth*4)))
-
-    #         for mi in tqdm(range(nmonth)):
-    #             for bi in range(4):
-
-    #                 emissions_perturbed = emissions.copy()
-    #                 emissions_perturbed[mi, bi] +=1
-
-    #                 mod.emissions = emissions_perturbed
-    #                 mod.run(verbose=False)
-    #                 mf_perturbed = mod.mf
-    #                 sensitivity[:, 4*mi + bi] = (mf_perturbed[:,:4].flatten() - mf_ref[:,:4].flatten()) / 1.
-
-    #     elif freq == "quarterly":
-    #         nquart = int(len(emissions)/3.)
-    #         sensitivity = np.zeros((len(mf_ref[:,:4].flatten()), int(nquart*4)))
-
-    #         for mi in tqdm(range(nquart)):
-    #             for bi in range(4):
-
-    #                 emissions_perturbed = emissions.copy()
-    #                 emissions_perturbed[mi*3:(3*mi+3), bi] +=1
-
-    #                 mod.emissions = emissions_perturbed
-    #                 mod.run(verbose=False)
-    #                 mf_perturbed = mod.mf
-    #                 sensitivity[:, 4*mi + bi] = (mf_perturbed[:,:4].flatten() - mf_ref[:,:4].flatten()) / 1.
+                # Run perturbed model
+                self.mod.run(verbose=False)
                 
-    # return sensitivity, mf_ref[:,:4].flatten(), emissions, mod.time, emis0
+                # Store sensitivity column
+                self.sensitivity[:, 4*ti + bi] = (self.mod.mf[:,:4].flatten() - self.mod_prior.mf[:,:4].flatten()) / 1.
 
 
 
