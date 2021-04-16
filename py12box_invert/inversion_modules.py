30455a3bef99a5206ccc30889b37336ef07ba2af
Luke Western <luke.western@bristol.ac.uk>
2021-04-16 09:02:22 +0100

    Functions to calculate annual emissions, mf and their uncertianties

--- py12box_invert/inversion_modules.py
+++ py12box_invert/inversion_modules.py
@@ -29,7 +29,8 @@ class Inverse_method:
                 P_hat (array): Posterior covariance matrix
         """
         R_inv = np.linalg.inv(self.mat.R)
-        self.mat.x_hat = np.linalg.inv(self.mat.H.T @ R_inv @ self.mat.H + self.mat.P_inv) @ (self.mat.H.T @ R_inv @ self.mat.y + self.mat.P_inv @ self.mat.x_a)
+        self.mat.x_hat = np.linalg.inv(self.mat.H.T @ R_inv @ self.mat.H + self.mat.P_inv) @ \
+                        (self.mat.H.T @ R_inv @ self.mat.y + self.mat.P_inv @ self.mat.x_a)
         self.mat.P_hat = np.linalg.inv(self.mat.H.T @ R_inv @ self.mat.H + self.mat.P_inv)
     
     
@@ -39,15 +40,48 @@ class Inverse_method:
         # Posterior mole fraction
         y_hat = self.sensitivity.sensitivity @ self.mat.x_hat + self.mod_prior.mf[:, :4].flatten()
         self.mod_posterior.mf = y_hat.reshape(int(len(y_hat)/4), 4)
+        R_hat = np.diag(self.sensitivity.sensitivity @ self.mat.P_hat @ self.sensitivity.sensitivity.T)
+        self.mod_posterior.mfuncertainty = np.sqrt(R_hat).reshape(int(len(y_hat)/4), 4)
 
         # Posterior emissions
         freq_months = self.sensitivity.freq_months
         self.mod_posterior.emissions = self.mod_prior.emissions.copy()
+        self.mod_posterior.emissionsuncertainty = np.zeros_like(self.mod_posterior.emissions)
         for ti in range(int(self.mod_prior.emissions.shape[0]/freq_months)):
             for bi in range(4):
-                self.mod_posterior.emissions[ti*freq_months:(ti+1)*freq_months, bi] += self.mat.x_hat[4*ti + bi]    
+                self.mod_posterior.emissions[ti*freq_months:(ti+1)*freq_months, bi] += self.mat.x_hat[4*ti + bi] 
+                self.mod_posterior.emissionsuncertainty[ti*freq_months:(ti+1)*freq_months, bi] = np.sqrt(self.mat.P_hat[4*ti + bi, 4*ti + bi])
+    
+    def analytical_gaussian_annualemissions(self):
+        """
+        Calculate annual total annual emissions and its uncertaity
+        """
+        freq_months = self.sensitivity.freq_months
+        n_months = int(12./self.sensitivity.freq_months)
+        self.mod_posterior.annualemissions = np.add.reduceat(self.mod_posterior.emissions.sum(axis=1), 
+                                                             np.arange(0, len(self.mod_posterior.emissions.sum(axis=1)), 12))/12
+        self.mod_posterior.annualemissionsuncertainty = np.zeros_like(self.mod_posterior.annualemissions)
+        for i in np.arange(len(self.mod_posterior.annualemissionsuncertainty)):
+            self.mod_posterior.annualemissionsuncertainty[i] = np.sqrt(self.mat.P_hat[(i*n_months*4):((i+1)*n_months*4),(i*n_months*4):((i+1)*n_months*4)].sum()/(n_months)**2)
 
-
+            
+    def analytical_gaussian_annualmf(self):
+        """
+        Calculate annual global and hemispheric mf with uncertianties
+        """
+        self.mod_posterior.annualmf = np.add.reduceat(self.mod_posterior.mf, np.arange(0,len(self.mod.time), 12), axis=0)/12.
+        self.mod_posterior.annualglobalmf = self.mod_posterior.annualmf.copy().mean(axis=1)
+        R_hat = self.sensitivity.sensitivity @ self.mat.P_hat @ self.sensitivity.sensitivity.T
+        self.mod_posterior.annualmfuncertainty = np.zeros_like(self.mod_posterior.annualmf)
+        self.mod_posterior.annualglobalmfuncertainty = np.zeros_like(self.mod_posterior.annualglobalmf)
+        for i in range(len(self.mod_posterior.annualglobalmf)):
+            self.mod_posterior.annualglobalmfuncertainty[i] = np.sqrt(R_hat[i*48:(i+1)*48].sum()/48**2)
+        for bi in range(4):
+            R_hat_bx = R_hat[bi::4,bi::4]
+            for i in range(self.mod_posterior.annualglobalmf.shape[0]):
+                self.mod_posterior.annualmfuncertainty[i,bi] = np.sqrt(R_hat_bx[i*12:(i+1)*12].sum()/12**2)
+        
+    
     def rigby14(self):
         """Emissions growth-constrainted method of Rigby et al., 2011 and 2014
         """
@@ -69,6 +103,7 @@ class Inverse_method:
     def iterative_rigby14(self):
         """
         As rigby14 but optimises x_hat such that all values are >=0. 
+        Assumes linearity in the uncertainty.
         """
         H = self.mat.H.copy()
         R_inv = np.linalg.inv(self.mat.R)
@@ -105,7 +140,7 @@ class Inverse_method:
         As iterative_rigby14 but optimises an iid model uncertainty. 
         i.e. there is an additional model error added to the measurement error, which is 
         assumed to be the same at all times and in all boxes.
-        It currently also assumes that the R matrix is diagonal to speed up have to do a matrix
+        It currently also assumes that the R matrix is diagonal to speed up having to do a matrix
         inversion at each step.
         """
         print("Caution: R matrix is assumed to be diagonal.")
@@ -153,19 +188,60 @@ class Inverse_method:
         """
 
         self.analytical_gaussian_posterior()
+        
+    def rigby14_annualemissions(self):
+        """
+        The same as a standard analytical Gaussian
+        """
+        
+        self.analytical_gaussian_annualemissions()
+        
+    def rigby14_annualmf(self):
+        """
+        The same as a standard analytical Gaussian
+        """
+        
+        self.analytical_gaussian_annualmf()
     
     def iterative_rigby14_posterior(self):
         """The same as a standard analytical Gaussian
         """
 
         self.analytical_gaussian_posterior()
+        
+    def iterative_rigby14_annualemissions(self):
+        """
+        The same as a standard analytical Gaussian
+        """
+        
+        self.analytical_gaussian_annualemissions()
+        
+    def iterative_rigby14_annualmf(self):
+        """
+        The same as a standard analytical Gaussian
+        """
+        
+        self.analytical_gaussian_annualmf()
 
     def empirical_bayes_posterior(self):
         """The same as a standard analytical Gaussian
         """
 
         self.analytical_gaussian_posterior()
-
+        
+    def empirical_bayes_annualemissions(self):
+        """
+        The same as a standard analytical Gaussian
+        """
+        
+        self.analytical_gaussian_annualemissions()
+        
+    def empirical_bayes_annualmf(self):
+        """
+        The same as a standard analytical Gaussian
+        """
+        
+        self.analytical_gaussian_annualmf()
 
         
         
