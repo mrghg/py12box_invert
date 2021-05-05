4373ee58410720ecd516225d47644586f80fb1bd
Luke Western <luke.western@bristol.ac.uk>
2021-05-05 12:25:02 +0100

    Plots growth rates with plot_mf()

--- py12box_invert/plot_altair.py
+++ py12box_invert/plot_altair.py
@@ -54,6 +54,8 @@ class Plot:
         xmax=data[np.isfinite(data["Obs"])]["Date"].max()
         ymin=data["Obs"].min()
         ymax=data["Obs"].max()
+        
+        x_domain = alt.selection_interval(bind='scales', encodings=['x'])
 
         # Calculate some y-ranges for the error bars
         base = alt.Chart(data).transform_calculate(
@@ -66,7 +68,7 @@ class Plot:
                 x=alt.X("Date:T",
                     scale=alt.Scale(domain=(xmin, xmax))),
                 y=alt.Y("Obs:Q",
-                    title="CFC-11 (ppt)",
+                    title=f"{self.species} (ppt)",
                     scale=alt.Scale(domain=(ymin, ymax))),
                 color="Box:N", 
                 opacity=alt.condition(selection, alt.value(0.8), alt.value(0.1)),
@@ -90,8 +92,63 @@ class Plot:
             opacity=alt.condition(selection, alt.value(1), alt.value(0.1))
             )
 
-        alt.layer(obs_plot, error_bars, mod_plot).add_selection(
+        mf_upper = alt.layer(obs_plot, error_bars, mod_plot).add_selection(
             selection
             ).properties(
                 width=600,
-                height=400).interactive().display()
+                height=400).interactive()
+        
+        #
+        # Plot the growth rate data
+        #
+        data_gr = pd.concat([pd.DataFrame(
+        data={"Date": dec_to_month(self.growth_rate.time),
+            "Box": box_name[bi],
+            "GR": self.growth_rate.mf[:, bi],
+            "GR_uncertainty": self.growth_rate.mfsd[:, bi],
+            "zero_line": np.zeros(len(self.growth_rate.time))}
+        ) for bi in range(4)])
+        
+        ymin_gr=data_gr["GR"].min()
+        ymax_gr=data_gr["GR"].max()
+
+        # Calculate some y-ranges for the error bars
+        base_gr = alt.Chart(data_gr).transform_calculate(
+            ymin_gr="datum.GR-datum.GR_uncertainty",
+            ymax_gr="datum.GR+datum.GR_uncertainty"
+        )
+
+        # Plot the zero line for clarity
+        gr_zeros = base_gr.mark_line(color="#000000").encode(
+                x=alt.X("Date:T"),
+                y=alt.Y("zero_line"))
+
+        # Plot the growth rate
+        gr_plot = base_gr.mark_line().encode( #mark_point(filled=True, size=5).encode(
+                x=alt.X("Date:T",
+                    scale=alt.Scale(domain=(xmin, xmax))),
+                y=alt.Y("GR:Q",
+                    title=f"Growth rate (ppt/yr)",
+                    scale=alt.Scale(domain=(ymin_gr, ymax_gr))),
+                color="Box:N", 
+                opacity=alt.condition(selection, alt.value(1), alt.value(0.1)),
+                tooltip=["Date", "GR"]
+        )
+
+        # Add error shading
+        gr_error_bars = base_gr.mark_area().encode(
+                x="Date:T",
+                y=alt.Y("ymin_gr:Q", title=""),
+                y2="ymax_gr:Q",
+                color="Box:N",
+                opacity=alt.condition(selection, alt.value(0.4), alt.value(0.1)))
+
+
+        gr_lower = alt.layer(gr_zeros,gr_error_bars,gr_plot).add_selection(
+        selection, x_domain
+        ).properties(
+            width=600,
+            height=150).interactive() 
+
+
+        alt.vconcat(mf_upper, gr_lower).display()  
